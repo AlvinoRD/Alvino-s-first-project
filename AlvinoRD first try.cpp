544985149b448273692aa5bb370b12dbce8a2a29
Alvino Radyo <111202214600@mhs.dinus.ac.id>
2022-09-12 12:31:15 +0700

    pertama

---
+++ AlvinoRD first try.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world! \nAlvino Radyo Danisworo \nA11.2022.14600 \nBanjarsari, Surakarta" << endl;
+
+    return 0;
+}
+
