9e550895cca9109ac9f4162afe171664b439d822
Alvino Radyo <111202214600@mhs.dinus.ac.id>
2022-09-19 14:08:48 +0700

    update biodata

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+/*
+Nama Alvino Radyo Danisworo
+NIM A11.2022.14600
+kelompok 4116
+Teknik Informatika
+*/
+
+using namespace std;
+
+int main()
+{
+    /* INI YANG AWAL AWAL HEHE */
+    int a = 18;
+    int b = 21;
+    int c = 17;
+    float t = 178.2;
+    float s = 42.5;
+    char u = 'L';
+    char d = 'L';
+    cout << "Umur Saya: " << a;
+    cout << endl;
+    cout << "Umur Kakak Saya: " << b;
+    cout << endl;
+    cout << "Umur Adik Saya: " << c;
+    cout << endl;
+    cout << "Tinggi Badan Saya: " << t;
+    cout << endl;
+    cout << "Ukuran Sepatu Saya: " << s;
+    cout << "Ukuran Baju Saya: " << u;
+    cout << endl;
+    cout << "Ukuran Celana Saya: " << d;
+    cout << endl;
+    cout << "SEKIAN TERIMA KASIH" << endl;
+
+    /* INI YANG KEREN */
+    int e;
+    cout << "masukkan angka anda: ";
+    cin >> e;
+    cout << "angka anda + c: " << e + c;
+
+    return 0;
+}
